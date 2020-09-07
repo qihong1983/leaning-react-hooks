5af2bd016bfe2094c7b05966e9691d27f9d05f69
qihong <qihong@winsense.ai>
2020-09-08 00:26:31 +0800

    aaa

--- src/App.js
+++ src/App.js
@@ -19,6 +19,14 @@ import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CounterTwo';
 import CounterThree from './components/CounterThree';
 import Counter from './components/Counter';
+import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
+import DocTitleOne from './components/DocTitleOne';
+import DocTitleTwo from './components/DocTitileTwo';
+import CustomCounterOne from './components/CustomCounterOne';
+import CustomCounterTwo from './components/CustomCounterTwo';
+import UserForm from './components/UserForm';
 
 
 export const UserContext = React.createContext();
@@ -31,7 +39,16 @@ function App() {
       {/* <CounterOne /> */}
       {/* <CounterThree /> */}
       {/* <CounterTwo /> */}
-      <Counter />
+      {/* <Counter /> */}
+      {/* <FocusInput /> */}
+      {/* <ClassTimer />
+      <HookTimer /> */}
+      {/* <DocTitleOne />
+      <DocTitleTwo /> */}
+
+      {/* <CustomCounterOne />
+      <CustomCounterTwo /> */}
+      <UserForm />
     </div>
   );
 }
---
+++ src/components/UserForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import useInput from '../hooks/useInput';
+
+function UserForm() {
+
+
+
+    const [firstName, bindFirstName, resetFirstName] = useInput('');
+    const [lastName, bindLastName, resetLastName] = useInput('');
+
+
+    console.log(bindFirstName, 'bindFirstName');
+    const submitHandler = (e) => {
+        e.preventDefault();
+        alert(`Hello ${firstName} ${lastName}`);
+
+        resetFirstName();
+        resetLastName();
+    }
+
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                <div>
+                    <label>First name</label>
+                    <input
+                        {...bindFirstName}
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <label>Last name</label>
+                    <input
+                        {...bindLastName}
+                        type="text"
+                    />
+                </div>
+                <button>submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default UserForm
